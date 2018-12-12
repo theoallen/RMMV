eb8c165a8965305617849c19bab284abfb6a497d
theoallen <theoallen@users.noreply.github.com>
2018-12-12 11:35:22 +0700

    Update TheoLimitedInventory.js

--- TheoLimitedInventory.js
+++ TheoLimitedInventory.js
@@ -1,9 +1,14 @@
 /*:
-@plugindesc Limits carrying capacity like in many popular games. This plugin is under construction
+@plugindesc Limits carrying capacity like in many popular games.
+Version 0.9
 @author TheoAllen
 @help
 This script allow you to limit your inventory by overall possesed 
 items instead of individual items.
+
+IMPORTANT NOTE:
+Name the file of this plugin literally as
+"TheoLimitedInventory.js"
 	
 Notetags :
 write down these notetags to the notebox in your database
@@ -27,7 +32,17 @@ check the script call instruction.
 <inv minus: n>
 Inverse of <inv plus>. It will decrease the avalaible inventory 
 slot. Should be self explanatory
-	
+
+TO DO LIST
+> Make it more touch input friendly
+> Add inventory slot eval formula
+> Maybe, some compatibility patch
+
+Terms of use :
+Credit me, TheoAllen. You are free to edit this script by your own. 
+As long as you don't claim it yours. For commercial purpose, don't 
+forget to give me a free copy of the game.
+
 @param Preferences
 	
 @param dynamic
@@ -106,7 +121,7 @@ slot. Should be self explanatory
 @parent Numeric Setting
 @text Use Command Size
 @type number
-@default 300
+@default 340
 @desc The width of use item command window
 
 @param Vocab Settings
@@ -114,37 +129,37 @@ slot. Should be self explanatory
 @param invSlotVocab
 @parent Vocab Settings
 @text Inventory Slot
-@default "Inventory:"
+@default Inventory:
 @desc Vocab for inventory
 
 @param invSizeVocab
 @parent Vocab Settings
 @text Size Vocab
-@default "Size:"
+@default Size:
 @desc Vocab for Size
 
 @param slotShort
 @parent Vocab Settings
 @text Inventory Slot (Short)
-@default "Inv:"
+@default Inv:
 @desc Vocab for inventory (short)
 
 @param useVocab
 @parent Vocab Settings
 @text Use Vocab
-@default "Use Item"
+@default Use Item
 @desc Vocab for using an item
 
 @param discardVocab
 @parent Vocab Settings
 @text Discard Vocab
-@default "Discard"
+@default Discard
 @desc Vocab for discarding an item
 
 @param cancelVocab
 @parent Vocab Settings
 @text Cancel Vocab
-@default "Cancel"
+@default Cancel
 @desc Vocab for canceling
 
  */ 
@@ -171,7 +186,7 @@ DataManager.isDatabaseLoaded = function(){
 				Theo.LINV.loadData(db);
 			})
 		})
-		$dataActors.forEach(function(a){if(a===null){return};console.log(a.invMod)});
+		$dataActors.forEach(function(a){if(!a){return};console.log(a.invMod)});
 		Theo.LINV.invLoaded = true;
 	}
     return true;
@@ -219,7 +234,7 @@ Game_Actor.prototype.setBaseInv = function(num){
 }
 
 Game_Actor.prototype.equipSize = function(){
-	if (!Theo.LINV.Params['includeEquip']){return 0};
+	if (Theo.LINV.Params['includeEquip'] === 'false'){return 0};
 	return this.equips().reduce(function(total, eq){
 		if(eq === null){return total + 0};
 		return total + eq.invSize;
@@ -253,7 +268,7 @@ Game_Actor.prototype.tradeItemWithParty = function(newItem, oldItem) {
 //===================================================================
 Theo.LINV.partyInit = Game_Party.prototype.initialize;
 Game_Party.prototype.initialize = function(){
-	this._baseInv = (Theo.LINV.Params['dynamic'] ? 0 : Theo.LINV.Params['defaultFree'])
+	this._baseInv = (Theo.LINV.Params['dynamic'] === 'true' ? 0 : Number(Theo.LINV.Params['defaultFree']))
 	Theo.LINV.partyInit.call(this);
 }
 
@@ -266,7 +281,7 @@ Game_Party.prototype.setBaseInv = function(num){
 }
 
 Game_Party.prototype.invMax = function(){
-	if(!Theo.LINV.Params['dynamic']){return this._baseInv};
+	if(Theo.LINV.Params['dynamic'] === 'false'){return this._baseInv};
 	return this.members().reduce(function(total, member){
 		return total + member.invMax();
 	}, 0) + this._baseInv;
@@ -277,9 +292,10 @@ Game_Party.prototype.isInvMaxed = function(){
 }
 
 Game_Party.prototype.totalInvSize = function(){
+	var numitem = this.numItems.bind(this);
 	var size = this.allItems().reduce(function(total, item){
-	if(item === null){return total + 0};
-		return total + item.invSize;
+	if(!item){return total + 0};
+		return total + item.invSize * numitem(item);
 	},0);
 	size += this.members().reduce(function(total, member){
 		return total + member.equipSize();
@@ -317,11 +333,11 @@ Game_Party.prototype.hasMaxItems = function(item) {
 };
 
 Game_Party.prototype.isNearingMax = function(){
-	return this.freeSlot() / this.invMax() <= Theo.LINV.Params['nearMaxPercent']/100
+	return this.freeSlot() / this.invMax() <= Number(Theo.LINV.Params['nearMaxPercent'])/100
 }
 
 Game_Party.prototype.itemSize = function(item){
-	if(!item || item === null){return 0};
+	if(!item){return 0};
 	return item.invSize * this.numItems(item);
 }
 
@@ -349,7 +365,7 @@ Game_Party.prototype.loseItem = function(item, amount, includeEquip) {
 // Overwrite
 Theo.LINV.partyGainItem = Game_Party.prototype.gainItem;
 Game_Party.prototype.gainItem = function(item, amount, includeEquip) {
-	if(Theo.LINV.Params['forceGain']){
+	if(Theo.LINV.Params['forceGain'] === 'true'){
 		Theo.LINV.partyGainItem.call(this, item, amount, includeEquip);
 	}else{
 		this.forceGainItem(item, amount, includeEquip);
@@ -361,7 +377,7 @@ Game_Party.prototype.gainItem = function(item, amount, includeEquip) {
 //===================================================================
 Theo.LINV.playerIsDashing = Game_Player.prototype.isDashing;
 Game_Player.prototype.isDashing = function() {
-	if(Theo.LINV.Params['fullDisableDash'] && $gameParty.isInvMaxed()){return false};
+	if(Theo.LINV.Params['fullDisableDash'] === 'true' && $gameParty.isInvMaxed()){return false};
   return Theo.LINV.playerIsDashing.call(this);
 };
 
@@ -372,13 +388,12 @@ Game_Player.prototype.realMoveSpeed = function() {
 };
 
 Game_Player.prototype.movePenalty = function(){
-	return (Theo.LINV.Params['fullSlowDown'] && $gameParty.isInvMaxed() ? 1 : 0);
+	return (Theo.LINV.Params['fullSlowDown'] === 'true' && $gameParty.isInvMaxed() ? 1 : 0);
 }
 //===================================================================
 // ** Window_Base
 //===================================================================
 Window_Base.prototype.drawInvSlot = function(x, y, width, align = 'right'){
-	// let txt = `${$gameParty.totalInvSize()}/${$gameParty.invMax()}`;
 	let total = String($gameParty.totalInvSize());
 	let invmax = String($gameParty.invMax());
 	let txt = total + "/" + invmax;
@@ -400,11 +415,14 @@ Window_Base.prototype.drawInvInfo = function(x,y,width){
 	this.drawInvSlot(x, y, width);
 }
 
-Window_Base.prototype.drawItemSize = function(item, x, y, width, total = true){
+Window_Base.prototype.drawItemSize = function(item, x, y, width){
 	this.changeTextColor(this.systemColor());
 	this.drawText(Theo.LINV.Params['invSizeVocab'], x, y, width);
 	this.changeTextColor(this.normalColor());
-	let num = (Theo.LINV.Params['drawTotalSize'] && total) ? $gameParty.itemSize(item) : item === null ? 0 : item.invSize;
+	// console.log($gameParty.itemSize(item));
+	// console.log(item.invSize);
+	let num = (Theo.LINV.Params['drawTotalSize'] === 'true') ? $gameParty.itemSize(item) : (!item ? 0 : item.invSize);
+	// console.log(num);
 	this.drawText(num, x, y, width, 'right');
 }
 //===================================================================
@@ -438,8 +456,8 @@ Theo.LINV.Window_ItemSize = function(){
 Theo.LINV.Window_ItemSize.prototype = Object.create(Window_Base.prototype);
 Theo.LINV.Window_ItemSize.prototype.constructor = Theo.LINV.Window_ItemSize;
 
-Theo.LINV.Window_ItemSize.prototype.initialize = function(width) {
-  Window_Base.prototype.initialize.call(this,0,0,width,this.fittingHeight(1));
+Theo.LINV.Window_ItemSize.prototype.initialize = function(x,y,width) {
+  Window_Base.prototype.initialize.call(this,x,y,width,this.fittingHeight(1));
 };
 
 Theo.LINV.Window_ItemSize.prototype.setItem = function(item) {
@@ -463,6 +481,7 @@ Theo.LINV.Window_FreeSlot.prototype.constructor = Theo.LINV.Window_FreeSlot;
 
 Theo.LINV.Window_FreeSlot.prototype.initialize = function(x,y,width) {
   Window_Base.prototype.initialize.call(this,x,y,width,this.fittingHeight(1));
+	this.refresh();
 };
 
 Theo.LINV.Window_FreeSlot.prototype.refresh = function(){
@@ -482,6 +501,7 @@ Theo.LINV.Window_ItemUse.prototype.constructor = Theo.LINV.Window_ItemUse;
 Theo.LINV.Window_ItemUse.prototype.initialize = function(){
 	Window_Command.prototype.initialize.call(this, 0, 0);
 	this._openness = 0;
+	this.deactivate();
 }
 
 Theo.LINV.Window_ItemUse.prototype.setItem = function(item){
@@ -490,7 +510,7 @@ Theo.LINV.Window_ItemUse.prototype.setItem = function(item){
 }
 
 Theo.LINV.Window_ItemUse.prototype.windowWidth = function() {
-	return Theo.LINV.Params[`commandSize`];
+	return Number(Theo.LINV.Params[`commandSize`]);
 };
 
 Theo.LINV.Window_ItemUse.prototype.makeCommandList = function(){
@@ -501,12 +521,12 @@ Theo.LINV.Window_ItemUse.prototype.makeCommandList = function(){
 
 Theo.LINV.Window_ItemUse.prototype.toCenter = function(){
 	this.x = Graphics.width/2 - this.width/2;
-	this.x = Graphics.height/2 - this.height/2;
+	this.y = Graphics.height/2 - this.height/2 - 20;
 }
 
 Theo.LINV.Window_ItemUse.prototype.discardAble = function(item){
 	if(!item) {return false};
-	if(item.itypeId) {return item.itypeId === 2}
+	if(item.itypeId) {return item.itypeId !== 2}
 	else{return true};
 }
 //===================================================================
@@ -521,8 +541,10 @@ Theo.LINV.Window_Discard.prototype.constructor = Theo.LINV.Window_Discard;
 
 Theo.LINV.Window_Discard.prototype.initialize = function(x,y,width){
 	Window_Base.prototype.initialize.call(this,x,y,width,this.fittingHeight(1));
-	this._openNess = 0;
+	this._openness = 0;
 	this._amount = 0;
+	this.setCursorRect(0, 0, this.contents.width, this.contents.height);
+	this.active = true;
 }
 
 Theo.LINV.Window_Discard.prototype.setItem = function(item){
@@ -534,20 +556,20 @@ Theo.LINV.Window_Discard.prototype.setItem = function(item){
 Theo.LINV.Window_Discard.prototype.refresh = function(){
 	this.contents.clear();
 	if(!this._item){return};
-	this.drawItemName(this._item, 0, 0, contents.width);
+	this.drawItemName(this._item, 2, 2, this.contents.width);
 	let txt = String(this._amount)+"/"+String($gameParty.numItems(this._item));
-	this.drawText(txt,0,0,this.contents.width,'right')
+	this.drawText(txt,0,1,this.contents.width-2,'right')
 }
 
 Theo.LINV.Window_Discard.prototype.drawItemName = function(item, x, y, width){
 	if(!item){return};
 	this.drawIcon(item.iconIndex, x, y);
-	this.drawText(item.name + ":",x + 24, y, width);
+	this.drawText(item.name + ":",x + 36, y, width);
 }
 
 Theo.LINV.Window_Discard.prototype.update = function(){
 	Window_Base.prototype.update.call(this);
-	if (!isOpen()){return};
+	if (!this.isOpen()){return};
 	if (Input.isRepeated('right')) {this.changeAmount(1)};
 	if (Input.isRepeated('left')) {this.changeAmount(-1)};
 	if (Input.isRepeated('up')) {this.changeAmount(10)};
@@ -557,7 +579,7 @@ Theo.LINV.Window_Discard.prototype.update = function(){
 }
 
 Theo.LINV.Window_Discard.prototype.changeAmount = function(amount){
-	this._amount = amount.clamp(0,$gameParty.numItems(this._item));
+	this._amount = (this._amount + amount).clamp(0,$gameParty.numItems(this._item));
 	SoundManager.playCursor();
 	this.refresh();
 }
@@ -596,7 +618,7 @@ Theo.LINV.itemList_updateHelp = Window_ItemList.prototype.updateHelp;
 Window_ItemList.prototype.updateHelp = function(){
 	Theo.LINV.itemList_updateHelp.call(this);
 	if (!this._itemSizeWindow){return};
-	this._itemSizeWindow.setItem(this.item);
+	this._itemSizeWindow.setItem(this.item());
 }
 
 Window_ItemList.prototype.changeHeight = function(height){
@@ -626,7 +648,7 @@ Window_ShopNumber.prototype.refresh = function(){
 Window_ShopNumber.prototype.drawItemSize = function(){
 	let num = this._number + this._item.invSize;
 	let total = $gameParty.totalInvSize() + (this._mode === 'buy' ? num : num * -1);
-	let txt = `${total}/${$gameParty.invMax()}`;
+	let txt = String(total)+"/"+String($gameParty.invMax());
 	let ypos = this.itemY() + lineHeight() * 4;
 	this.changeTextColor(this.systemColor());
 	this.drawText(Theo.LINV.Params['invSlotVocab'],6,ypos,contents.width-12);
@@ -636,7 +658,7 @@ Window_ShopNumber.prototype.drawItemSize = function(){
 //===================================================================
 // ** Default Script - Window_ShopStatus
 //===================================================================
-if (Theo.LINV.Params['displayItem']){
+if (Theo.LINV.Params['displayItem'] === 'true'){
 	Theo.LINV.shopStatusPossession = Window_ShopStatus.prototype.drawPossession;
 	Window_ShopStatus.prototype.drawPossession = function(x, y){
 		Theo.LINV.shopStatusPossession.call(this, x, y);
@@ -667,60 +689,87 @@ Theo.LINV.sceneItemCreate = Scene_Item.prototype.create;
 Scene_Item.prototype.create = function(){
 	Theo.LINV.sceneItemCreate.call(this);
 	this.resizeItemWindow();
-  	this.createFreeslotWindow();
-  	this.createItemsizeWindow();
-  	this.createUsecommandWindow();
-  	this.createDiscardAmount();
+	this.createUpperLayer();
+	this.createFreeslotWindow();
+	this.createItemsizeWindow();
+	this.createUsecommandWindow();
+	this.createDiscardAmount();
+	this.reoderChilds();
 }
 
 Scene_Item.prototype.resizeItemWindow = function(){
 	this._itemWindow.height -= this._itemWindow.lineHeight() * 2;
 }
 
+Scene_Item.prototype.reoderChilds = function(){
+	this._windowLayer.children.findIndex(function(element){
+		if (element === this._actorWindow){
+			this._windowLayer.removeChild(element);
+			this._windowLayer.addChild(element);
+			return true;
+		}
+		return false;
+	},this);
+}
+
+Scene_Item.prototype.createUpperLayer = function(){
+	let width = Graphics.boxWidth;
+	let height = Graphics.boxHeight;
+	let x = (Graphics.width - width) / 2;
+	let y = (Graphics.height - height) / 2;
+	this._useLayer = new WindowLayer();
+	this._useLayer.move(x, y, width, height);
+	this.addChild(this._useLayer);
+}
+
+Scene_Item.prototype.addUpperWindow = function(w){
+	this._useLayer.addChild(w);
+}
+
 Scene_Item.prototype.createFreeslotWindow = function(){
 	let wy = this._itemWindow.y + this._itemWindow.height;
-  	let wh = Theo.LINV.Params['displayItem'] ? Graphics.width/2 : Graphics.width;
-  	this._freeSlot = new Theo.LINV.Window_FreeSlot(0,wy,wh);
-  	this.addWindow(this._freeslot);
+	let wh = Theo.LINV.Params['displayItem'] === 'true' ? Graphics.width/2 : Graphics.width;
+	this._freeSlot = new Theo.LINV.Window_FreeSlot(0,wy,wh);
+	this.addWindow(this._freeSlot);
 }
 
 Scene_Item.prototype.createItemsizeWindow = function(){
-	if(!Theo.LINV.Params['displayItem']){return};
-	let wx = this._freeslot.width
-  	let wy = this._freeslot.y
-  	let ww = wx
-  	this._itemSize = new Theo.LINV.Window_ItemSize(wx,wy,ww)
-	this._itemWindow.itemSizeWindow = this._itemSize
+	if(Theo.LINV.Params['displayItem'] === 'false'){return};
+	let wx = this._freeSlot.width
+	let wy = this._freeSlot.y
+	let ww = wx
+	this._itemSize = new Theo.LINV.Window_ItemSize(wx,wy,ww)
+	this._itemWindow._itemSizeWindow = this._itemSize
 	this.addWindow(this._itemSize)
 }
 
 Scene_Item.prototype.createUsecommandWindow = function(){
 	this._useCommand = new Theo.LINV.Window_ItemUse()
-  this._useCommand.toCenter()
+	this._useCommand.toCenter()
 	this._useCommand.setHandler('use', this.useCommandOk.bind(this))
 	this._useCommand.setHandler('discard', this.onDiscardOk.bind(this))
-  this._useCommand.setHandler('cancel', this.onUseCancel.bind(this))
-	this.addWindow(this._useCommand)
+	this._useCommand.setHandler('cancel', this.onUseCancel.bind(this))
+	this.addUpperWindow(this._useCommand)
 }
 
 Scene_Item.prototype.createDiscardAmount = function(){
 	let wx = this._useCommand.x
-  	let wy = this._useCommand.y + this._useCommand.height
-  	let ww = this._useCommand.width
-  	this._discardWindow = new Theo.LINV.Window_Discard(wx,wy,ww)
-  	this._discardWindow.commandWindow = this._useCommand
-  	this._discardWindow.itemList = this._itemWindow
-  	this._discardWindow.freeSlot = this._freeSlot
-  	this.addWindow(this._discardWindow)
+	let wy = this._useCommand.y + this._useCommand.height
+	let ww = this._useCommand.width
+	this._discardWindow = new Theo.LINV.Window_Discard(wx,wy,ww)
+	this._discardWindow.commandWindow = this._useCommand
+	this._discardWindow.itemList = this._itemWindow
+	this._discardWindow.freeSlot = this._freeSlot
+	this.addUpperWindow(this._discardWindow)
 }
 
 // Overwrite
 Theo.LINV.sceneItem_OnItemOk = Scene_Item.prototype.onItemOk;
 Scene_Item.prototype.onItemOk = function(){
 	this._useCommand.setItem(this.item())
-  	this._useCommand.open()
-  	this._useCommand.activate()
-  	this._useCommand.select(0)
+	this._useCommand.open()
+	this._useCommand.activate()
+	this._useCommand.select(0)
 }
 
 Theo.LINV.sceneItem_UseItem = Scene_Item.prototype.useItem;
